4b809c4ed15dfbcde1455d8ba79d28e7e7664e2f
David Matthew Mattli <dmm@mattli.us>
2022-01-26 15:55:05 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+pub mod stream;
+
+use std::error::Error;
+
+use act_zero::*;
+use async_trait::async_trait;
+use futures::executor::LocalPool;
+use futures_util::stream::once;
+
+use crate::stream::StreamHandler;
+
+struct SimpleGreeter {
+    number_of_greets: i32,
+}
+
+#[async_trait]
+impl Actor for SimpleGreeter {
+    async fn started(&mut self, addr: Addr<Self>) -> ActorResult<()> {
+        println!("Starting stream actor!");
+        Self::add_stream(once(async { String::from("Mary") }), addr);
+        Produces::ok(())
+    }
+}
+
+#[async_trait]
+impl StreamHandler<String> for SimpleGreeter {
+    async fn started2(&mut self) {
+        println!("Stream has started!");
+    }
+
+    async fn handle(&mut self, item: String) {
+        println!("Got stream item: {}", item);
+        if let Ok(Produces::Value(greeting)) = self.greet(item).await {
+            println!("{}", greeting);
+        }
+    }
+
+    async fn finished(&mut self) {
+        println!("Stream has stopped!");
+    }
+}
+
+impl SimpleGreeter {
+    async fn greet(&mut self, name: String) -> ActorResult<String> {
+        self.number_of_greets += 1;
+        Produces::ok(format!(
+            "Hello, {}. You are number {}!",
+            name, self.number_of_greets
+        ))
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut pool = LocalPool::new();
+    let spawner = pool.spawner();
+    pool.run_until(async move {
+        let actor_ref = Addr::new(
+            &spawner,
+            SimpleGreeter {
+                number_of_greets: 0,
+            },
+        )?;
+
+        let greeting = call!(actor_ref.greet("John".into())).await?;
+        println!("{}", greeting);
+
+        let greeting = call!(actor_ref.greet("Emma".into())).await?;
+        println!("{}", greeting);
+
+        Ok(())
+    })
+}
---
+++ src/stream.rs
@@ -0,0 +1,92 @@
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use act_zero::*;
+use async_trait::async_trait;
+use futures::{ready, stream::Stream, Future};
+use pin_project_lite::pin_project;
+
+#[async_trait]
+pub trait StreamHandler<I>
+where
+    Self: Actor,
+{
+    async fn handle(&mut self, item: I);
+
+    async fn started2(&mut self) {}
+
+    async fn finished(&mut self) {}
+
+    fn add_stream<S>(stream: S, addr: Addr<Self>)
+    where
+        S: Stream + Send + 'static,
+        Self: StreamHandler<S::Item> + Sized,
+        S::Item: Send + Clone,
+    {
+        let addr2 = addr.clone();
+        addr2.send_fut(ActorStream::new(stream, addr))
+    }
+}
+
+pin_project! {
+    pub struct ActorStream<S, A>
+    where
+S: Stream,
+A: StreamHandler<S::Item>,
+{
+        #[pin]
+        stream: S,
+        actor: Addr<A>,
+        started: bool,
+    }
+}
+
+impl<S, A> ActorStream<S, A>
+where
+    S: Stream,
+    A: StreamHandler<S::Item>,
+{
+    pub fn new(fut: S, actor: Addr<A>) -> Self {
+        Self {
+            stream: fut,
+            actor,
+            started: false,
+        }
+    }
+}
+
+impl<S, A> Future for ActorStream<S, A>
+where
+    S: Stream,
+    S::Item: Clone + Send + 'static,
+    A: StreamHandler<S::Item>,
+{
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, task: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut this = self.project();
+
+        if !*this.started {
+            *this.started = true;
+            let a = this.actor.as_addr();
+            //send!(<a as StreamHandler>::started());
+            send!(a.started2());
+        }
+
+        let mut polled = 0;
+
+        while let Some(msg) = ready!(this.stream.as_mut().poll_next(task)) {
+            let a = this.actor.as_addr();
+            send!(a.handle(msg));
+
+            polled += 1;
+
+            if polled == 16 {
+                return Poll::Pending;
+            }
+        }
+        let a = this.actor.as_addr();
+        send!(a.finished());
+        Poll::Ready(())
+    }
+}
